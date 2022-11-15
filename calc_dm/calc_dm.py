cdaa3e09fefab8d07ea941fc6fc2a858ed9168a8
kaz-mintan <kumagai@mizuuchi.lab.tuat.ac.jp>
2022-11-15 16:00:18 +0900

    updated

--- calc_dm/calc_dm.py
+++ calc_dm/calc_dm.py
@@ -6,7 +6,7 @@ for user_id in range(9):
   mental=np.loadtxt(mental_filename,delimiter=",")
   print(mental)
   dm=np.diff(mental)
-  dm_filename="./"+str(user_id+1)+"_para/dm_calc.csv"
-  #np.savetxt(dm_filename,dm,delimiter=",")
+  dm_filename="../data/"+str(user_id+1)+"/dm_calc.csv"
+  np.savetxt(dm_filename,dm,delimiter=",",fmt='%.3f')
   print(dm)
 
