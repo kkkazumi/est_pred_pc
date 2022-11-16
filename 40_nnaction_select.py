d9fb90e2c4050a128cd2a381f5fc910aa98d6c82
kaz-mintan <kumagai@mizuuchi.lab.tuat.ac.jp>
2022-11-16 18:04:06 +0900

    add candidate select and calculate factor

--- 40_nnaction_select.py
+++ 40_nnaction_select.py
@@ -1,9 +1,3 @@
-import optuna
-
-import keras.backend as K
-from keras.models import Sequential, Model, load_model
-from keras.layers import Input, Dense, Activation
-from keras.layers.advanced_activations import LeakyReLU
 import numpy as np
 import scipy as sp
 from scipy.stats import pearsonr
@@ -14,11 +8,40 @@ OUTPUT_DIM = 1
 EPOCH = 50000
 
 for name_num in (0,1,2,3,4,5,6,7,8):
-  dir_name = "./jrm_test/" + str(name_num+1)
-
+  dir_name = "./data/" + str(name_num+1)
   #test to output
-  factor_test = np.loadtxt(dir_name + "/factor_train.csv",delimiter='\t')
-  mood_test = np.loadtxt(dir_name + "/mood_train" + i_csv ,delimiter=',')
+  factor_test = np.loadtxt(dir_name + "/factor_test.csv",delimiter='\t')
+  #TODO get mood
+  #mood_test = np.loadtxt(dir_name + "/mood_train" + i_csv ,delimiter=',')
+
+  target_row = 2#to 6j:w
+  target_len = 5
+
+  for t in range(1,10):
+    if(t<5):
+      before_action_count = factor_test[t-1,target_row:target_row+target_len]*t
+      for action in range(5):
+        arr = np.zeros_like(before_action_count)
+        arr[action]=1
+        _cand = before_action_count + arr 
+        action_cand=_cand/float(t+1)
+        print("action cand",action_cand)
+    else:
+      before_action_count = factor_test[t-1,target_row:target_row+target_len]*5
+      for action in range(5):
+        arr = np.zeros_like(before_action_count)
+        arr[action]=1
+        _cand = before_action_count + arr 
+        action_cand=_cand/5.0
+        print("action cand",action_cand)
+    input()
+
+  import optuna
+  import keras.backend as K
+  from keras.models import Sequential, Model, load_model
+  from keras.layers import Input, Dense, Activation
+  from keras.layers.advanced_activations import LeakyReLU
+
 
   m_pred = np.zeros_like(mood_test)
 
